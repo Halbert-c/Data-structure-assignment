8cc7f2c549b941f7d4c33c1448363b529900ad37
Jacey <22050587@imail.sunway.edu.my>
2024-12-27 17:03:23 +0800

    Update btree.py

--- btree.py
+++ btree.py
@@ -1,22 +1,22 @@
 import tkinter as tk
 from tkinter import Canvas, ttk
-
+from tkinter import messagebox
 class BTreeNode:
-    def __init__(self, t, is_leaf):
-        self.t = t  # Minimum degree
+    def __init__(self, max_keys, is_leaf):
+        self.max_keys = max_keys  # Maximum keys a node can hold
         self.is_leaf = is_leaf  # True if leaf node
         self.keys = []  # List of keys
         self.children = []  # List of children
 
 class BTree:
-    def __init__(self, t):
-        self.root = BTreeNode(t, True)
-        self.t = t  # Minimum degree
+    def __init__(self, max_degree):
+        self.root = BTreeNode(max_degree-1 , True)
+        self.max_degree = max_degree  # Maximum number of children a node can have
 
     def insert(self, key):
         root = self.root
-        if len(root.keys) == 2 * self.t - 1:
-            new_root = BTreeNode(self.t, False)
+        if len(root.keys) == root.max_keys:  # Split when the node is full
+            new_root = BTreeNode(self.max_degree-1, False)
             new_root.children.append(self.root)
             self._split_child(new_root, 0)
             self.root = new_root
@@ -24,133 +24,169 @@ class BTree:
         else:
             self._insert_non_full(root, key)
 
-    def delete(self, key):
-        if self.root:
-            self._delete(self.root, key)
-            if len(self.root.keys) == 0:  # Shrink the tree
-                if not self.root.is_leaf:
-                    self.root = self.root.children[0]
-                else:
-                    self.root = None
+    def _split_child(self, parent, index):
+        # Get the full child node
+        child = parent.children[index]
+        max_keys = child.max_keys
+        middle_idx = max_keys // 2
+        middle_key = child.keys[middle_idx]
 
-    def _delete(self, node, key):
-        t = self.t
-        if key in node.keys:
-            if node.is_leaf:
-                node.keys.remove(key)
-            else:
-                idx = node.keys.index(key)
-                if len(node.children[idx].keys) >= t:
-                    pred = self._get_predecessor(node, idx)
-                    node.keys[idx] = pred
-                    self._delete(node.children[idx], pred)
-                elif len(node.children[idx + 1].keys) >= t:
-                    succ = self._get_successor(node, idx)
-                    node.keys[idx] = succ
-                    self._delete(node.children[idx + 1], succ)
-                else:
-                    self._merge(node, idx)
-                    self._delete(node.children[idx], key)
-        else:
-            if node.is_leaf:
-                return
-            idx = 0
-            while idx < len(node.keys) and key > node.keys[idx]:
-                idx += 1
-            if len(node.children[idx].keys) < t:
-                self._fill(node, idx)
-            if idx < len(node.keys) and key > node.keys[idx]:
-                idx += 1
-            self._delete(node.children[idx], key)
+        # Create a new child node
+        new_child = BTreeNode(child.max_keys, child.is_leaf)
 
-    def reset(self):
-        self.root = BTreeNode(self.t, True)
+        # Split the keys and children
+        new_child.keys = child.keys[middle_idx + 1:]
+        child.keys = child.keys[:middle_idx]
 
-    def _get_predecessor(self, node, idx):
-        current = node.children[idx]
-        while not current.is_leaf:
-            current = current.children[-1]
-        return current.keys[-1]
+        if not child.is_leaf:
+            new_child.children = child.children[middle_idx + 1:]
+            child.children = child.children[:middle_idx + 1]
 
-    def _get_successor(self, node, idx):
-        current = node.children[idx + 1]
-        while not current.is_leaf:
-            current = current.children[0]
-        return current.keys[0]
-
-    def _merge(self, parent, idx):
-        child = parent.children[idx]
-        sibling = parent.children[idx + 1]
-        child.keys.append(parent.keys.pop(idx))
-        child.keys.extend(sibling.keys)
-        if not sibling.is_leaf:
-            child.children.extend(sibling.children)
-        parent.children.pop(idx + 1)
-
-    def _fill(self, parent, idx):
-        t = self.t
-        if idx != 0 and len(parent.children[idx - 1].keys) >= t:
-            self._borrow_from_prev(parent, idx)
-        elif idx != len(parent.children) - 1 and len(parent.children[idx + 1].keys) >= t:
-            self._borrow_from_next(parent, idx)
-        else:
-            if idx != len(parent.children) - 1:
-                self._merge(parent, idx)
-            else:
-                self._merge(parent, idx - 1)
-
-    def _borrow_from_prev(self, parent, idx):
-        child = parent.children[idx]
-        sibling = parent.children[idx - 1]
-        child.keys.insert(0, parent.keys[idx - 1])
-        parent.keys[idx - 1] = sibling.keys.pop()
-        if not sibling.is_leaf:
-            child.children.insert(0, sibling.children.pop())
-
-    def _borrow_from_next(self, parent, idx):
-        child = parent.children[idx]
-        sibling = parent.children[idx + 1]
-        child.keys.append(parent.keys[idx])
-        parent.keys[idx] = sibling.keys.pop(0)
-        if not sibling.is_leaf:
-            child.children.append(sibling.children.pop(0))
+        # Insert the middle key into the parent node
+        parent.keys.insert(index, middle_key)
+        parent.children.insert(index + 1, new_child)
 
     def _insert_non_full(self, node, key):
         i = len(node.keys) - 1
+
+        # If the node is a leaf, find the correct position to insert the key
         if node.is_leaf:
             while i >= 0 and key < node.keys[i]:
                 i -= 1
             node.keys.insert(i + 1, key)
         else:
+            # If not a leaf, find the correct child node to insert into
             while i >= 0 and key < node.keys[i]:
                 i -= 1
-            i += 1
-            if len(node.children[i].keys) == 2 * self.t - 1:
-                self._split_child(node, i)
+            i += 1  # i will be the index of the child to recurse into
+
+            # Check if the child node is full
+            if len(node.children[i].keys) == node.children[i].max_keys:
+                self._split_child(node, i)  # Split the child if it's full
+
+                # After splitting, check the key against the parent node to decide which child to recurse into
                 if key > node.keys[i]:
                     i += 1
+
+            # Recurse into the appropriate child node
             self._insert_non_full(node.children[i], key)
+    def delete(self, key):
+        self._delete(self.root, key)
+        if len(self.root.keys) == 0 and not self.root.is_leaf:
+            self.root = self.root.children[0]  # Update root if it's empty
+    def _delete(self, node, key):
+        if key in node.keys:  # Key found in the current node
+            if node.is_leaf:
+                node.keys.remove(key)  # If it's a leaf, simply remove the key
+            else:
+                idx = node.keys.index(key)
 
-    def _split_child(self, parent, index):
-        t = self.t
-        child = parent.children[index]
-        new_child = BTreeNode(t, child.is_leaf)
+                # Case 1: The left child has enough keys to borrow
+                if len(node.children[idx].keys) >= self.max_degree // 2:
+                    predecessor = self._get_predecessor(node, idx)
+                    node.keys[idx] = predecessor
+                    self._delete(node.children[idx], predecessor)
 
-        # Move the middle key of the full child to the parent
-        middle_key = child.keys[t - 1]
-        parent.keys.insert(index, middle_key)
+                # Case 2: The right child has enough keys to borrow
+                elif len(node.children[idx + 1].keys) >= self.max_degree // 2:
+                    successor = self._get_successor(node, idx)
+                    node.keys[idx] = successor
+                    self._delete(node.children[idx + 1], successor)
 
-        # Create the new child node
-        parent.children.insert(index + 1, new_child)
+                # Case 3: Both children are too small, so merge the children
+                else:
+                    self._merge(node, idx)
+                    self._delete(node.children[idx], key)
+        else:
+            # Key is not in the current node, continue searching in the children
+            idx = self._find_index(node, key)
+            if node.is_leaf:
+                return  # Key not found, do nothing
+            if len(node.children[idx].keys) < self.max_degree // 2:
+                self._fill(node, idx)
+            self._delete(node.children[idx], key)
 
-        # Assign keys to the new child
-        new_child.keys = child.keys[t:]
-        child.keys = child.keys[:t - 1]
+    def _get_predecessor(self, node, idx):
+        curr = node.children[idx]
+        while not curr.is_leaf:
+            curr = curr.children[-1]  # Go to the rightmost child
+        return curr.keys[-1]
 
-        # If the child is not a leaf, adjust its children
-        if not child.is_leaf:
-            new_child.children = child.children[t:]
-            child.children = child.children[:t]
+    def _get_successor(self, node, idx):
+        curr = node.children[idx + 1]
+        while not curr.is_leaf:
+            curr = curr.children[0]  # Go to the leftmost child
+        return curr.keys[0]
+
+    def _merge(self, node, idx):
+        child = node.children[idx]
+        sibling = node.children[idx + 1]
+        child.keys.append(node.keys[idx])  # Move parent key down to the child
+        child.keys.extend(sibling.keys)  # Merge sibling keys into the child
+
+        if not sibling.is_leaf:
+            child.children.extend(sibling.children)  # Merge sibling children into the child
+
+        node.keys.pop(idx)  # Remove the parent key
+        node.children.pop(idx + 1)  # Remove the sibling
+
+    def _fill(self, node, idx):
+        if idx > 0 and len(node.children[idx - 1].keys) >= self.max_degree // 2:
+            self._borrow_from_prev(node, idx)
+        elif idx < len(node.children) - 1 and len(node.children[idx + 1].keys) >= self.max_degree // 2:
+            self._borrow_from_next(node, idx)
+        else:
+            if idx < len(node.children) - 1:
+                self._merge(node, idx)
+            else:
+                self._merge(node, idx - 1)
+
+    def _borrow_from_prev(self, node, idx):
+        child = node.children[idx]
+        sibling = node.children[idx - 1]
+
+        child.keys.insert(0, node.keys[idx - 1])  # Borrow a key from the left sibling
+        if not sibling.is_leaf:
+            child.children.insert(0, sibling.children.pop())  # Borrow a child from the left sibling
+
+        node.keys[idx - 1] = sibling.keys.pop()  # Move the sibling's key up to the parent
+
+    def _borrow_from_next(self, node, idx):
+        child = node.children[idx]
+        sibling = node.children[idx + 1]
+
+        child.keys.append(node.keys[idx])  # Borrow a key from the right sibling
+        if not sibling.is_leaf:
+            child.children.append(sibling.children.pop(0))  # Borrow a child from the right sibling
+
+        node.keys[idx] = sibling.keys.pop(0)  # Move the sibling's key up to the parent
+
+    def _find_index(self, node, key):
+        i = 0
+        while i < len(node.keys) and key > node.keys[i]:
+            i += 1
+        return i
+        
+    def search(self, key):
+        return self._search(self.root, key)
+    
+    def _search(self, node, key):
+        # Iterate through node keys
+        i = 0
+        while i < len(node.keys) and key > node.keys[i]:
+            i += 1
+
+        if i < len(node.keys) and key == node.keys[i]:  # Key found
+            return node
+        
+        if node.is_leaf:  # If leaf, key is not found
+            return None
+        
+        # Otherwise, recurse into the appropriate child node
+        return self._search(node.children[i], key)
+    
+    def reset(self):
+        self.root = BTreeNode(self.max_degree - 1, True)
 
 class BTreeVisualizer:
     def __init__(self, tree):
@@ -165,11 +201,10 @@ class BTreeVisualizer:
         self.control_frame = tk.Frame(self.window)
         self.control_frame.pack()
 
-        self.degree_label = tk.Label(self.control_frame, text="Degree:")
+        self.degree_label = tk.Label(self.control_frame, text="Max Degree:")
         self.degree_label.pack(side=tk.LEFT)
 
-        self.degree_combobox = ttk.Combobox(self.control_frame, values=[2, 3, 4, 5], state="readonly")
-        self.degree_combobox.set(2)
+        self.degree_combobox = ttk.Combobox(self.control_frame, values=[3, 4, 5, 6], state="readonly")
         self.degree_combobox.pack(side=tk.LEFT)
         self.degree_combobox.bind("<<ComboboxSelected>>", self.set_degree)
 
@@ -179,16 +214,21 @@ class BTreeVisualizer:
         self.insert_button = tk.Button(self.control_frame, text="Insert", command=self.insert_key)
         self.insert_button.pack(side=tk.LEFT)
 
+        self.clear_button = tk.Button(self.control_frame, text="Search", command=self.search_key)
+        self.clear_button.pack(side=tk.LEFT)
+
         self.delete_button = tk.Button(self.control_frame, text="Delete", command=self.delete_key)
         self.delete_button.pack(side=tk.LEFT)
 
         self.clear_button = tk.Button(self.control_frame, text="Clear", command=self.clear_canvas)
         self.clear_button.pack(side=tk.LEFT)
+        
+        self.node_positions = {}
 
     def set_degree(self, event):
         try:
             new_degree = int(self.degree_combobox.get())
-            self.tree.t = new_degree
+            self.tree.max_degree = new_degree
             self.tree.reset()
             self.draw_tree()
         except ValueError:
@@ -201,21 +241,36 @@ class BTreeVisualizer:
         else:
             self.canvas.create_text(400, 300, text="Tree is empty", font=("Arial", 14))
 
+    
     def _draw_node(self, node, x, y, x_offset):
+        if len(node.keys) == 0: 
+            return
+
         self._draw_circle(x, y, node.keys)
+
         if not node.is_leaf:
             num_children = len(node.children)
-            # Adjust x_offset if necessary to ensure proper spacing
+            
+            if num_children == 1:
+                child_x_start = x
+            else:
+                
+                total_width = x_offset * (num_children - 1)
+                child_x_start = x - total_width // 2  
             for i, child in enumerate(node.children):
-                child_x = x - (x_offset // 2) + i * (x_offset // num_children)
-                child_y = y + 100
-                self.canvas.create_line(x, y, child_x, child_y, fill="black")
-                self._draw_node(child, child_x, child_y, x_offset // 2)
+                
+                child_x = child_x_start + i * x_offset
+                child_y = y + 100 
 
+                if len(child.keys) > 0:  
+                    self.canvas.create_line(x, y, child_x, child_y, fill="black")
+                    
+                    self._draw_node(child, child_x, child_y, x_offset // 2)  
     def _draw_circle(self, x, y, keys):
         r = self.node_radius
         self.canvas.create_oval(x - r, y - r, x + r, y + r, fill="lightblue")
         self.canvas.create_text(x, y, text=",".join(map(str, keys)), font=("Arial", 10))
+
 
     def insert_key(self):
         try:
@@ -225,8 +280,6 @@ class BTreeVisualizer:
             self.draw_tree()
         except ValueError:
             pass
-        finally:
-            self.entry.delete(0, tk.END)
 
     def delete_key(self):
         try:
@@ -236,11 +289,20 @@ class BTreeVisualizer:
             self.draw_tree()
         except ValueError:
             pass
+    def search_key(self):
+        try:
+            key = int(self.entry.get())
+            result = self.tree.search(key)
+            if result:
+                messagebox.showinfo("Search Result", f"Key {key} found in the tree!")
+            else:
+                messagebox.showinfo("Search Result", f"Key {key} not found in the tree.")
+        except ValueError:
+            messagebox.showerror("Error", "Please enter a valid integer")
         finally:
-            self.entry.delete(0, tk.END)
-
+            self.entry.delete(0,tk.END)
     def clear_canvas(self):
-        self.tree.reset()  
+        self.tree.reset()
         self.canvas.delete("all")  # Clear the canvas visuals
         self.draw_tree()
 
@@ -248,10 +310,6 @@ class BTreeVisualizer:
         self.window.mainloop()
 
 if __name__ == "__main__":
-    # Create a B-Tree with minimum degree 2
-    btree = BTree(2)
-
-    # Visualize the B-Tree
-    visualizer = BTreeVisualizer(btree)
-    visualizer.draw_tree()
+    tree = BTree(max_degree=3)
+    visualizer = BTreeVisualizer(tree)
     visualizer.run()
